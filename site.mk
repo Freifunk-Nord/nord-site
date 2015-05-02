5a5a54a15a34655b478c2b95df54157196fddd8d
ruben <ruben.git@r.z11.de>
2015-05-02 12:05:27 +0200

    initial commit

---
+++ site.mk
@@ -0,0 +1,40 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-14 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-setup-mode \
+	gluon-config-mode-core \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-legacy \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged
+
+
+DEFAULT_GLUON_RELEASE := 0.7-exp
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_BRANCH ?= experimental
+export GLUON_BRANCH
+
+GLUON_TARGET ?= ar71xx-generic
+export GLUON_TARGET
