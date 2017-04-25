879cd0dae1b8542cbc993bc1542ce9a068a5089b
Tarnatos <tarnatos@posteo.de>
2017-04-25 21:32:10 +0200

    gluon-config-mode-hostname hinzugefügt

--- site.mk
+++ site.mk
@@ -4,6 +4,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-respondd \
 	gluon-autoupdater \
 	gluon-setup-mode \
+	gluon-config-mode-hostname \
 	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-mesh-vpn \
