9622dba5a1cafb8e10e68ecf2cb2ab3ea8b2ca32
Tarnatos <tarnatos@posteo.de>
2017-04-26 18:16:30 +0200

    LEDE: lede au branch hinzugefügt

--- site.mk
+++ site.mk
@@ -46,13 +46,13 @@ GLUON_SITE_PACKAGES := \
 
 # Always call `make` from the command line with the desired release version!
 # otherwise this is generated:
-DEFAULT_GLUON_RELEASE := 2017.0.0~lede$(shell date '+%y%m%d%H%M')
+DEFAULT_GLUON_RELEASE := 2017.01.0~lede$(shell date '+%y%m%d%H%M')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 0
-GLUON_BRANCH ?= experimental
+GLUON_BRANCH ?= lede
 export GLUON_BRANCH
 
 GLUON_TARGET ?= ar71xx-generic
