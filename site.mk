5d6c0c50539911af89c533bd005b3df31a44918e
Tarnatos <tarnatos@posteo.de>
2017-04-27 19:49:56 +0200

    LEDE: DEFAULT_GLUON_RELEASE 2017.0.0

--- site.mk
+++ site.mk
@@ -46,7 +46,7 @@ GLUON_SITE_PACKAGES := \
 
 # Always call `make` from the command line with the desired release version!
 # otherwise this is generated:
-DEFAULT_GLUON_RELEASE := 2017.01.0~lede$(shell date '+%y%m%d%H%M')
+DEFAULT_GLUON_RELEASE := 2017.0.0~lede$(shell date '+%y%m%d%H%M')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
