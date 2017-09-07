2fdc691c0c36649f02ca04ecbb4309142bc25b26
rubo77 <github@r.z11.de>
2017-09-07 02:12:55 +0200

    v2016.2.7

--- site.mk
+++ site.mk
@@ -49,8 +49,9 @@ GLUON_SITE_PACKAGES += \
 
 # Always call `make` from the command line with the desired release version!
 # otherwise this is generated:
-DEFAULT_GLUON_RELEASE := 2016.2.7~exp$(shell date '+%y%m%d%H%M')
-#DEFAULT_GLUON_RELEASE := 2016.2.7
+#DEFAULT_GLUON_RELEASE := 2016.2.7~exp$(shell date '+%y%m%d%H%M')
+#DEFAULT_GLUON_RELEASE := 2016.2.7~exp
+DEFAULT_GLUON_RELEASE := 2016.2.7
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
