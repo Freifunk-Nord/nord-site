52ef604206ee8ceabb656f82024d4df838ad9500
Tarnatos <tarnatos@posteo.de>
2017-11-16 15:33:49 +0100

    gluon 2017.1.4

--- site.mk
+++ site.mk
@@ -45,8 +45,8 @@ GLUON_SITE_PACKAGES += \
 
 # Always call `make` from the command line with the desired release version!
 # otherwise this is generated:
-#DEFAULT_GLUON_RELEASE := 2017.1.3~exp$(shell date '+%y%m%d%H%M')
-DEFAULT_GLUON_RELEASE := 2017.1.3~exp
+#DEFAULT_GLUON_RELEASE := 2017.1.4~exp$(shell date '+%y%m%d%H%M')
+DEFAULT_GLUON_RELEASE := 2017.1.4
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
