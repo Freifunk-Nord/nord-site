4bc7d53814f5529499146cc0fc56e55e4e353d89
rubo77 <github@r.z11.de>
2018-07-24 01:43:13 +0200

    add USB for Gl.inet gl-ar300m

--- site.mk
+++ site.mk
@@ -183,6 +183,7 @@ ifeq ($(GLUON_TARGET),ar71xx-generic)
 	GLUON_buffalo-wzr-hp-g450h_SITE_PACKAGES := $(USB_PACKAGES_STORAGE)
 	GLUON_buffalo-wzr-hp-g300nh_SITE_PACKAGES := $(USB_PACKAGES_STORAGE)
 	GLUON_tp-link-archer-c7-v2_SITE_PACKAGES := $(USB_PACKAGES_STORAGE)
+	GLUON_gl-ar300m_SITE_PACKAGES := $(USB_PACKAGES_STORAGE)
 endif
 
 ifeq ($(GLUON_TARGET),mpc85xx-generic)
