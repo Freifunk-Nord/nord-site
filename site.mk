a58fe26664399bb90e2334d63bd2ea736963d059
rubo77 <github@r.z11.de>
2018-10-15 07:41:35 +0200

    add disabled ddhcpd package

--- site.mk
+++ site.mk
@@ -48,8 +48,8 @@ GLUON_SITE_PACKAGES := \
 
 # from sargon:
 GLUON_SITE_PACKAGES += \
-	roamguide
-#	ddhcpd
+	roamguide \
+	ddhcpd
 
 # from https://github.com/Freifunk-Nord/eulenfunk-packages
 GLUON_SITE_PACKAGES += \
