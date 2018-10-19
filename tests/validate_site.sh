291a8079e6e4ec1860c7ca02281ed27d27862e5d
rubo77 <github@r.z11.de>
2018-10-19 04:07:35 +0200

    validate script gehärtet gegen string-injections

--- tests/validate_site.sh
+++ tests/validate_site.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -uo pipefail
 
 # validate_site.sh checks if the site.conf is valid json
 GLUON_REPO="https://github.com/freifunk-gluon/gluon"
@@ -6,7 +7,7 @@ GLUON_BRANCH='master'
 GLUON_PACKAGES_REPO="https://github.com/freifunk-gluon/packages"
 GLUON_PACKAGES_BRANCH='master'
 
-P=$(pwd)
+P="$(pwd)"
 echo "####### validating $P/site.conf ..."
 which lua5.1 
 if [ "$?" == 1 ]; then
@@ -20,22 +21,23 @@ if [ "$?" == 1 ]; then
 fi
 
 echo "####### validating $P/make-release.sh ..."
-bash -n $P/make-release.sh
+bash -n "$P/make-release.sh"
 
 echo "####### validating $P/modules ..."
-source $P/modules
+GLUON_SITE_FEEDS="none"
+source "$P/modules"
 testpath=/tmp/site-validate
-rm -Rf $testpath
-mkdir -p $testpath/packages
-cd $testpath/packages
+rm -Rf "$testpath"
+mkdir -p "$testpath/packages"
+cd "$testpath/packages"
 for feed in $GLUON_SITE_FEEDS; do
   echo "####### checking PACKAGES_${feed^^}_REPO ..."
-  repo_var=$(echo PACKAGES_${feed^^}_REPO)
-  commit_var=$(echo PACKAGES_${feed^^}_COMMIT)
-  branch_var=$(echo PACKAGES_${feed^^}_BRANCH)
-  repo=${!repo_var}
-  commit=${!commit_var}
-  branch=${!branch_var}
+  repo_var="PACKAGES_${feed^^}_REPO"
+  commit_var="PACKAGES_${feed^^}_COMMIT"
+  branch_var="PACKAGES_${feed^^}_BRANCH"
+  repo="${!repo_var}"
+  commit="${!commit_var}"
+  branch="${!branch_var}"
   if [ "$repo" == "" ]; then
     echo "repo $repo_var missing"
     exit 1
@@ -48,9 +50,9 @@ for feed in $GLUON_SITE_FEEDS; do
     echo "branch $branch_var missing"
     exit 1
   fi
-  git clone -b "$branch" --depth 1000 --single-branch "$repo" $feed
+  git clone -b "$branch" --depth 1000 --single-branch "$repo" "$feed"
   if [ "$?" != "0" ]; then exit 1; fi
-  cd $feed
+  cd "$feed"
   echo "git checkout $commit"
   git checkout "$commit"
   if [ "$?" != "0" ]; then exit 1; fi
@@ -58,37 +60,37 @@ for feed in $GLUON_SITE_FEEDS; do
 done
 
 echo "####### downloading $GLUON_PACKAGES_REPO ..."
-git clone -b $GLUON_PACKAGES_BRANCH --depth 1  --single-branch $GLUON_PACKAGES_REPO
+git clone -b "$GLUON_PACKAGES_BRANCH" --depth 1  --single-branch "$GLUON_PACKAGES_REPO"
 
 echo "####### downloading gluon ..."
-cd $testpath
+cd "$testpath"
 git init gluon
 cd gluon
-git remote add origin $GLUON_REPO
+git remote add origin "$GLUON_REPO"
 git config core.sparsecheckout true
 echo "package/*" >> .git/info/sparse-checkout
-git pull --depth 1 origin $GLUON_BRANCH
-cp -a package/ $testpath/packages
-cd $testpath/packages/package
+git pull --depth 1 origin "$GLUON_BRANCH"
+cp -a package/ "$testpath/packages"
+cd "$testpath/packages/package"
 
 echo "####### validating GLUON_SITE_PACKAGES from $P/site.mk ..."
 # ignore non-gluon packages and standard gluon features
-sed '0,/^GLUON_LANGS/d' $P/site.mk | sed '/GLUON_TARGET/,$d' | egrep -v '(#|G|iwinfo|iptables|haveged|vim|socat|tar|mesh-batman-adv-1[45]|web-advanced|web-wizard|-gluon-)'> $testpath/site.mk.sh
-sed -i 's/\s\\$//g;/^$/d' $testpath/site.mk.sh
-sed -i 's/gluon-mesh-batman-adv-1[45]/gluon-mesh-batman-adv/g' $testpath/site.mk.sh
+sed '0,/^GLUON_LANGS/d' "$P/site.mk" | sed '/GLUON_TARGET/,$d' | egrep -v '(#|G|iwinfo|iptables|haveged|vim|socat|tar|mesh-batman-adv-1[45]|web-advanced|web-wizard)'> "$testpath"/site.mk.sh
+sed -i 's/\s\\$//g;/^$/d' "$testpath"/site.mk.sh
+sed -i 's/gluon-mesh-batman-adv-1[45]/gluon-mesh-batman-adv/g' "$testpath"/site.mk.sh
 sed -i 's/gluon-config-mode-geo-location-with-map/gluon-config-mode-geo-location/g' $testpath/site.mk.sh
-cat $testpath/site.mk.sh |
+cat "$testpath"/site.mk.sh |
 while read packet; do
   if [ "$packet" != "" ]; then
     echo -n "# $packet"
-    FOUND="$(find $testpath/packages/ -type d -name "$packet")"
+    FOUND="$(find "$testpath/packages/" -type d -name "$packet")"
     if [ "$FOUND" '!=' '' ]; then
-      echo " found as feature in $(echo $FOUND|sed 's|'$testpath'/packages||g')"
+      echo " found as feature in $(echo "$FOUND"|sed 's|'"$testpath/packages"'||g')"
     else
       # check again with prefix gluon-
-      FOUND="$(find $testpath/packages/ -type d -name "gluon-$packet")"
+      FOUND="$(find "$testpath/packages/" -type d -name "gluon-$packet")"
       if [ "$FOUND" '!=' '' ]; then
-        echo " found in $(echo $FOUND|sed 's|'$testpath'/packages||g')"
+        echo " found in $(echo "$FOUND"|sed 's|'"$testpath/packages"'||g')"
       else
         echo
         echo "ERROR: $packet missing"
