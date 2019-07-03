f550d89e3597093f8d4ca356f887ba73f80d3ed5
Ruben Barkow <ruben.barkow@eclabs.de>
2019-07-03 03:14:57 +0000

    build more targets # Conflicts: #	.gitlab-ci/build.sh

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -12,7 +12,7 @@
 # =====================================================================
 
 # Default make options (override with -m)
-MAKEOPTS="-j 4"
+MAKEOPTS="-j 4 GLUON_DEPRECATED=upgrade"
 
 # Default is set to use current work directory
 SITEDIR="$(pwd)"
@@ -168,7 +168,7 @@ if [[ -z ${TARGETS+x} ]] ; then
       TARGETS="ar71xx-tiny ar71xx-generic"
       TARGETS+=" x86-64 x86-generic" # (VMs)
       TARGETS+=" ar71xx-nand" # (Netgear WNDR3700, WNDR4300, ZyXEL NBG6716)
-      TARGETS+=" mpc85xx-generic" # (tp-link-tl-wdr4900-v1)
+      #TARGETS+=" mpc85xx-generic" # (tp-link-tl-wdr4900-v1)
       TARGETS+=" ramips-mt7620" # (gl-inet mt300 und mt750)
       #TARGETS+=" sunxi-cortexa7" # (Banana Pi M1)
 
@@ -177,8 +177,8 @@ if [[ -z ${TARGETS+x} ]] ; then
       TARGETS+=" ipq40xx" # (FitzBox 4040)
       #TARGETS+=" ramips-mt7621" # (D-Link DIR-860L (B1) Ubiquiti EdgeRouter X, ZBT WG3526)
       TARGETS+=" x86-geode"
-      #TARGETS+=" ramips-rt305x" # BROKEN: (fonera, vocore a5)
-      TARGETS+=" ramips-mt76x8" # BROKEN: unstable WiFi (tp-link 841 v13 und archer c50)
+      TARGETS+=" ramips-rt305x" # BROKEN: (fonera, vocore a5)
+      TARGETS+=" ramips-mt76x8" # BROKEN: unstable WiFi (tp-link 841 v13, Netgear R6120 und archer c50)
       #TARGETS+=" ar71xx-mikrotik" # BROKEN: no sysupgrade support (mikrotik-nand)
       #TARGETS+=" brcm2708-bcm2710" # BROKEN: Untested (raspberry-pi-3)
       #TARGETS+=" ipq806x" # BROKEN: unstable wifi drivers (tp-link-archer-c2600)
